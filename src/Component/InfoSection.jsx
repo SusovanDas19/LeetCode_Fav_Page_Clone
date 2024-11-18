9c9129274eca3c552e04d16e724a089257eea68a
Susovan Das <amesusovan@gmail.com>
2024-11-18 16:49:20 +0530

    Modify circle

--- src/Component/InfoSection.jsx
+++ src/Component/InfoSection.jsx
@@ -7,8 +7,17 @@ import { IoMdRefresh } from "react-icons/io";
 import { FcCheckmark } from "react-icons/fc";
 import { useState } from "react";
 import { motion } from "framer-motion";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import {
+  easyHoverAtom,
+  hardHoverAtom,
+  medHoverAtom,
+} from "../atoms/difficultyHover";
 
 function InfoSection() {
+  const setEasyHover = useSetRecoilState(easyHoverAtom);
+  const setMedHover = useSetRecoilState(medHoverAtom);
+  const setHardHover = useSetRecoilState(hardHoverAtom);
   return (
     <div className="flex flex-col gap-2.5 rounded-xl justify-center items-center bg-[#262626] w-96 h-[550px] ml-14">
       <div className="flex items-start gap-2.5 self-start flex-col mx-6">
@@ -51,15 +60,27 @@ function InfoSection() {
             </div>
           </div>
           <div className="flex basic-[30%] self-start flex-col gap-3">
-            <div className="flex text-slate-200 bg-[#333333] w-24 h-14 items-center justify-center flex-col p-4 rounded-md">
+            <div
+              className="flex text-slate-200 bg-[#333333] w-24 h-14 items-center justify-center flex-col p-4 rounded-md"
+              onMouseEnter={() => setEasyHover(true)}
+              onMouseLeave={() => setEasyHover(false)}
+            >
               <p className="text-[#4CE9EC]">Easy</p>
               <p>8/8</p>
             </div>
-            <div className="flex text-slate-200 bg-[#333333] w-24 h-14 items-center justify-center flex-col p-4 rounded-md">
+            <div
+              className="flex text-slate-200 bg-[#333333] w-24 h-14 items-center justify-center flex-col p-4 rounded-md"
+              onMouseEnter={() => setMedHover(true)}
+              onMouseLeave={() => setMedHover(false)}
+            >
               <p className="text-[#F7B529]	">Med.</p>
               <p>11/11</p>
             </div>
-            <div className="flex text-slate-200 bg-[#333333] w-24 h-14 items-center justify-center flex-col p-4 rounded-md">
+            <div
+              className="flex text-slate-200 bg-[#333333] w-24 h-14 items-center justify-center flex-col p-4 rounded-md"
+              onMouseEnter={() => setHardHover(true)}
+              onMouseLeave={() => setHardHover(false)}
+            >
               <p className="text-[#F3493F]	">Hard</p>
               <p>2/2</p>
             </div>
@@ -84,6 +105,9 @@ const CircularProgressBar = ({
   const segment3 = (circumference * 7.14) / 100; // Third segment
   const segment4 = (circumference * 74.99) / 100;
   const [hover, setHover] = useState(false);
+  const easyHover = useRecoilValue(easyHoverAtom);
+  const medHover = useRecoilValue(medHoverAtom);
+  const hardHover = useRecoilValue(hardHoverAtom);
 
   return (
     <div
@@ -91,124 +115,262 @@ const CircularProgressBar = ({
       onMouseLeave={() => setHover(false)}
     >
       <svg width={size} height={size} className="-rotate-225">
-        {hover ? (
+        {!easyHover && !medHover && !hardHover ? (
           <>
-            {/* First Segment */}
-            <motion.circle
-              initial={{
-                strokeDasharray: `0 ${circumference}`,
-                stroke: "transparent",
-              }}
-              animate={{
-                strokeDasharray: `${segment1} ${circumference - segment1}`,
-                stroke: colors[0],
-              }}
-              transition={{ duration: 0.8, ease: "easeInOut" }}
-              cx={size / 2}
-              cy={size / 2}
-              r={radius}
-              strokeWidth={strokeWidth}
-              fill="none"
-              strokeLinecap="round"
-              strokeDashoffset= {0}
-            />
-            {/* Second Segment */}
-            <motion.circle
-              initial={{
-                strokeDasharray: `0 ${circumference}`,
-                stroke: "transparent",
-              }}
-              animate={{
-                strokeDasharray: `${segment2} ${circumference - segment2}`,
-                stroke: colors[1],
-              }}
-              transition={{ duration: 0.8, delay: 0.2, ease: "easeInOut" }}
-              cx={size / 2}
-              cy={size / 2}
-              r={radius}
-              strokeWidth={strokeWidth}
-              fill="none"
-              strokeLinecap="round"
-              strokeDashoffset= {-segment1}
-            />
-            {/* Third Segment */}
-            <motion.circle
-              initial={{
-                strokeDasharray: `0 ${circumference}`,
-                stroke: "transparent",
-              }}
-              animate={{
-                strokeDasharray: `${segment3} ${circumference - segment3}`,
-               
-                stroke: colors[2],
-              }}
-              transition={{ duration: 0.8, delay: 0.8, ease: "easeInOut" }}
-              cx={size / 2}
-              cy={size / 2}
-              r={radius}
-              strokeWidth={strokeWidth}
-              fill="none"
-              strokeLinecap="round"
-              strokeDashoffset={-(segment1 + segment2)}
-            />
-            <circle
-              transition={{ duration: 0.8, delay: 0.8, ease: "easeInOut" }}
-              cx={size / 2}
-              cy={size / 2}
-              r={radius}
-              stroke="transparent"
-              strokeWidth={strokeWidth}
-              fill="none"
-            />
+            {hover ? (
+              <>
+                {/* First Segment */}
+                <motion.circle
+                  initial={{
+                    strokeDasharray: `0 ${circumference}`,
+                    stroke: "transparent",
+                  }}
+                  animate={{
+                    strokeDasharray: `${segment1} ${circumference - segment1}`,
+                    stroke: colors[0],
+                  }}
+                  transition={{ duration: 0.8, ease: "easeInOut" }}
+                  cx={size / 2}
+                  cy={size / 2}
+                  r={radius}
+                  strokeWidth={strokeWidth}
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeDashoffset={0}
+                />
+                {/* Second Segment */}
+                <motion.circle
+                  initial={{
+                    strokeDasharray: `0 ${circumference}`,
+                    stroke: "transparent",
+                  }}
+                  animate={{
+                    strokeDasharray: `${segment2} ${circumference - segment2}`,
+                    stroke: colors[1],
+                  }}
+                  transition={{ duration: 0.8, delay: 0.2, ease: "easeInOut" }}
+                  cx={size / 2}
+                  cy={size / 2}
+                  r={radius}
+                  strokeWidth={strokeWidth}
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeDashoffset={-segment1}
+                />
+                {/* Third Segment */}
+                <motion.circle
+                  initial={{
+                    strokeDasharray: `0 ${circumference}`,
+                    stroke: "transparent",
+                  }}
+                  animate={{
+                    strokeDasharray: `${segment3} ${circumference - segment3}`,
+                    stroke: colors[2],
+                  }}
+                  transition={{ duration: 0.8, delay: 0.8, ease: "easeInOut" }}
+                  cx={size / 2}
+                  cy={size / 2}
+                  r={radius}
+                  strokeWidth={strokeWidth}
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeDashoffset={-(segment1 + segment2)}
+                />
+              </>
+            ) : (
+              <>
+                <motion.circle
+                  initial={{
+                    strokeDasharray: `0 ${circumference}`,
+                  }}
+                  animate={{
+                    strokeDasharray: `${segment4} ${circumference - segment4}`,
+                    strokeDashoffset: 0,
+                  }}
+                  transition={{ duration: 0.5 }}
+                  cx={size / 2}
+                  cy={size / 2}
+                  r={radius}
+                  stroke={colors[3]}
+                  strokeWidth={strokeWidth}
+                  fill="none"
+                  strokeLinecap="round"
+                />
+              </>
+            )}
           </>
         ) : (
           <>
-            <motion.circle
-              initial={{
-                strokeDasharray: `0 ${circumference}`,
-              }}
-              animate={{
-                strokeDasharray: `${segment4} ${circumference - segment4}`,
-                strokeDashoffset: 0,
-              }}
-              transition={{ duration: 0.8 }}
-              cx={size / 2}
-              cy={size / 2}
-              r={radius}
-              stroke= {colors[3]}
-              strokeWidth={strokeWidth}
-              fill="none"
-              strokeLinecap="round"
-            />
+            {easyHover ? (
+              <>
+                <circle
+                  cx={size / 2}
+                  cy={size / 2}
+                  r={radius}
+                  stroke="#565858"
+                  strokeDasharray={`${circumference + 20}`}
+                  strokeDashoffset={`${segment1}`}
+                  strokeWidth={strokeWidth}
+                  fill="none"
+                  strokeLinecap="round"
+                />
+                <motion.circle
+                  initial={{
+                    strokeDasharray: `0 ${circumference}`,
+                  }}
+                  animate={{
+                    strokeDasharray: `${segment1} ${circumference - segment1}`,
+                    strokeDashoffset: 0,
+                  }}
+                  transition={{ duration: 0.5 }}
+                  cx={size / 2}
+                  cy={size / 2}
+                  r={radius}
+                  stroke={colors[0]}
+                  strokeWidth={strokeWidth}
+                  fill="none"
+                  strokeLinecap="round"
+                />
+              </>
+            ) : null}
+            {medHover ? (
+              <>
+                <circle
+                  cx={size / 2}
+                  cy={size / 2}
+                  r={radius}
+                  stroke="#565858"
+                  strokeDasharray={`${circumference + 20}`}
+                  strokeDashoffset={`${segment1}`}
+                  strokeWidth={strokeWidth}
+                  fill="none"
+                  strokeLinecap="round"
+                />
+                <motion.circle
+                  initial={{
+                    strokeDasharray: `0 ${circumference}`,
+                  }}
+                  animate={{
+                    strokeDasharray: `${segment2} ${circumference - segment2}`,
+                    strokeDashoffset: 0,
+                  }}
+                  transition={{ duration: 0.5 }}
+                  cx={size / 2}
+                  cy={size / 2}
+                  r={radius}
+                  stroke={colors[1]}
+                  strokeWidth={strokeWidth}
+                  fill="none"
+                  strokeLinecap="round"
+                />
+              </>
+            ) : null}
+            {hardHover ? (
+              <>
+                <circle
+                  cx={size / 2}
+                  cy={size / 2}
+                  r={radius}
+                  stroke="#565858"
+                  strokeDasharray={`${circumference + 20}`}
+                  strokeDashoffset={`${segment1}`}
+                  strokeWidth={strokeWidth}
+                  fill="none"
+                  strokeLinecap="round"
+                />
+                <motion.circle
+                  initial={{
+                    strokeDasharray: `0 ${circumference}`,
+                  }}
+                  animate={{
+                    strokeDasharray: `${segment3} ${circumference - segment3}`,
+                    strokeDashoffset: 0,
+                  }}
+                  transition={{ duration: 0.5 }}
+                  cx={size / 2}
+                  cy={size / 2}
+                  r={radius}
+                  stroke={colors[2]}
+                  strokeWidth={strokeWidth}
+                  fill="none"
+                  strokeLinecap="round"
+                />
+              </>
+            ) : null}
           </>
         )}
       </svg>
 
       <div className="flex items-center top-1/3 left-1/2 absolute -translate-x-1/2 -translate-y-1/2">
-        <div className="flex justify-center items-end">
+        <motion.div
+          className="flex justify-center items-end"
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 0.1,
+            delay: 0.7,
+            ease: [0, 0.71, 0.2, 1.01],
+          }}
+        >
           {hover ? (
             <>
               <h1 className="text-4xl font-bold">73</h1>
               <p>.93%</p>
             </>
-          ) : (
+          ) : null}
+
+          {easyHover ? (
+            <>
+              <h1 className="text-4xl font-bold">76</h1>
+              <p>.88%</p>
+            </>
+          ) : medHover ? (
+            <>
+              <h1 className="text-4xl font-bold">76</h1>
+              <p>.95%</p>
+            </>
+          ) : hardHover ? (
+            <>
+              <h1 className="text-4xl font-bold">51</h1>
+              <p>.06%</p>
+            </>
+          ) : null}
+
+          {!hover && !easyHover && !medHover && !hardHover ? (
             <>
               <h1 className="text-4xl font-bold">21</h1>
               <p>/21</p>
             </>
-          )}
-        </div>
+          ) : null}
+        </motion.div>
       </div>
-      <div className="flex flex-row items-center justify-center absolute top-20 left-10 gap-1">
-        {hover ? (
-          <p>Acceptance</p>
+      <motion.div
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
+          duration: 0.1,
+          delay: 0.7,
+          ease: [0, 0.71, 0.2, 1.01],
+        }}
+        className="flex flex-row items-center justify-center absolute top-20 left-10 gap-1"
+      >
+        {easyHover || medHover || hardHover ? (
+          <>
+            <p className="ml-6">Beats</p>
+          </>
         ) : (
           <>
-            <FcCheckmark />
-            <p>Solved</p>
+            {hover ? (
+              <p>Acceptance</p>
+            ) : (
+              <>
+                <FcCheckmark />
+                <p>Solved</p>
+              </>
+            )}
           </>
         )}
-      </div>
+      </motion.div>
       <div className="flex items-center absolute bottom-4 left-10 w-auto text-[#787878]">
         <p>0 Attempting</p>
       </div>
---
+++ src/atoms/difficultyHover.js
@@ -0,0 +1,14 @@
+import { atom } from "recoil";
+
+export const easyHoverAtom = atom({
+    key: "easyHover",
+    default: false
+})
+export const medHoverAtom = atom({
+    key: "medHover",
+    default: false
+})
+export const hardHoverAtom = atom({
+    key: "hardHover",
+    default: false
+})
