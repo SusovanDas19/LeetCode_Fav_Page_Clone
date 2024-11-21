edc15c986c4be3ee7b29730ba6c9fca4d682b646
Susovan Das <amesusovan@gmail.com>
2024-11-21 20:23:48 +0530

    Add Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,22 @@ import { RecoilRoot } from "recoil";
 import "./App.css";
 import InfoSection from "./Component/InfoSection";
 import QuestionsPart from "./Component/Questions";
+import Sidebar from "./Component/Sidebar";
 
 function App() {
   return (
     <RecoilRoot>
-      <div className=" flex flex-row h-screen max-w-full bg-[#1a1a1a] mx-6 gap-5">
-        <div className=" flex h-full w-1/3 mt-20 fixed top-1 left-6 ">
-          <InfoSection />
+      <div className=" flex flex-row h-screen max-w-full bg-[#1a1a1a]">
+        <div className=" h-full w-fit">
+          <Sidebar />
         </div>
-        <div className="flex h-full w-2/3 mt-20 ml-[33%]">
-          <QuestionsPart />
+        <div className=" h-full w-11/12 flex flex-row mx-6">
+          <div className=" flex h-full w-1/4 mt-20 top-10 sticky">
+            <InfoSection />
+          </div>
+          <div className="flex w-3/4 mt-20 ml-44 justify-start">
+            <QuestionsPart />
+          </div>
         </div>
       </div>
     </RecoilRoot>
---
+++ src/Component/Sidebar.jsx
@@ -0,0 +1,62 @@
+import { useRecoilState, useSetRecoilState } from "recoil";
+import { sidebarAtom } from "../atoms/sidebarAtom";
+import { PiSidebarFill } from "react-icons/pi";
+import { MdOutlineStarPurple500 } from "react-icons/md";
+import { CiLock } from "react-icons/ci";
+
+
+
+function Sidebar() {
+  const [showSidebar, setShowSidebar] = useRecoilState(sidebarAtom);
+  return (
+    <div className="h-full w-full">
+      {showSidebar ? (
+        <>
+          <div className=" flex h-full flex-col w-56 border-r justify-center border-[#484747] bg-[#333333] relative">
+            <div className="flex h-9  absolute top-0 left-0 right-0 items-center mx-6 mt-3  justify-between">
+              <h1 className="text-base text-slate-50 font-semibold">
+                My Lists
+              </h1>
+
+              <div
+                className="cursor-pointer flex items-center justify-center  "
+                onClick={() => {
+                  setShowSidebar(false); //this is not working, the div is not cursor pointer showing
+                }}
+              >
+                <PiSidebarFill
+                  className="text-slate-50 text-xl"
+                  strokeWidth="10"
+                />
+              </div>
+            </div>
+            <div className="absolute top-10 left-0 right-0 mx-6 mt-3">
+              <p className="text-sm text-slate-50 font-semibold">
+                Created by me
+              </p>
+            </div>
+            <div className="absolute  hover:bg-[#434343] h-12 top-20 left-0 right-0 mx-4 mt-3 flex items-center justify-between rounded-md">
+              <div className="bg-white h-7 w-7 flex justify-center items-center rounded ml-2">
+                <MdOutlineStarPurple500 className="fill-[#f2b04f] text-lg" />
+              </div>
+              <p className="text-sm text-slate-50 font-semibold mr-16">Favorite</p>
+              <CiLock className="text-slate-50 text-lg mr-2" strokeWidth="1"/>
+            </div>
+          </div>
+        </>
+      ) : (
+        <div className="bg-[#1a1a1a] mt-2 ml-3 h-7 w-8 flex items-center justify-center border-x border-y border-[#373737] rounded cursor-pointer">
+          <PiSidebarFill
+            className="text-slate-50 text-xl "
+            strokeWidth="10"
+            onClick={() => {
+              setShowSidebar(true);
+            }}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Sidebar;
